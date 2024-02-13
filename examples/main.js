792f7ce3deac1340345788b520f4954a67a50b02
mondodavid <david@madebymondo.com>
2024-02-13 01:11:01 -1000

    feat: add demo listener for 'togglenav' event

--- examples/main.js
+++ examples/main.js
@@ -1,3 +1,10 @@
-import './elements/nma-elements-row.js'
+import "./elements/nma-elements-row.js";
 import "./elements/nma-burger.js";
 
+window.addEventListener(
+  "togglenav",
+  () => {
+    console.log("A web component has dispatched a 'togglenav' event");
+  },
+  false
+);
