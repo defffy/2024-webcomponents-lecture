4a6c113d007c19576f6a541490c31da688bbace8
mondodavid <david@madebymondo.com>
2024-02-14 00:24:28 -1000

    fix: change 'togglenav' example to use alert

--- examples/main.js
+++ examples/main.js
@@ -1,11 +1,11 @@
 import "./elements/nma-elements-row.js";
 import "./elements/nma-burger.js";
-import './elements/nma-lightbox.js'
+import "./elements/nma-lightbox.js";
 
 window.addEventListener(
   "togglenav",
   () => {
-    console.log("A web component has dispatched a 'togglenav' event");
+    alert("A web component has dispatched a 'togglenav' event");
   },
   false
 );
